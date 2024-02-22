e2ca5a480ceb05feeae4a9da7b91a61aeab49044
ferdi <pedriibee1802@gmail.com>
2024-02-22 08:59:38 -0800

    commit pertama

---
+++ kalkulator.js
@@ -0,0 +1,12 @@
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function calculateResult() {
+    let result = eval(document.getElementById('display').value);
+    document.getElementById('display').value = result;
+}
